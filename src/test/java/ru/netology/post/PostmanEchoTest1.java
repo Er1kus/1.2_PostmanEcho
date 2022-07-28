e1321917b05f4cc519e9979fef43a1e1d3ba6466
Er1kus <goryaev86@mail.ru>
2022-07-29 01:34:57 +0300

    Initial failed commit

---
+++ src/test/java/ru/netology/post/PostmanEchoTest1.java
@@ -0,0 +1,24 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest1 {
+    @Test
+    void shouldSendAndReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .header("Content-length", "426")
+                .header("Content-Type", "application/json; charset=utf-9");
+    }
+}
